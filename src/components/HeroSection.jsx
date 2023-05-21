b7d3d0e38f4b978a6c3780dca200a8ee7881b19b
unknown <jayprakashpj2525@gmail.com>
2023-05-21 13:38:41 +0530

    about us responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
 <div className='w-full  bg-[#F3F7F5]   rounded-3xl flex flex-col justify-between'>
 <div className='grid grid-cols-1  gap-6 md:gap-24 md:grid-cols-2 max-w-[1240px] m-auto'>
 
-<div className='flex flex-col justify-center  md:items-start md:ml-16 w-full '>
+<div className='flex flex-col justify-center  md:items-start md:ml-16 w-full pt-12 '>
   <h1 className='mt-11 md:mt-0 md:mb-8  lg:max-w-[605px] lg:text-6xl lg:leading-[69px] md:text-4xl md:tracking-wide md:max-w-md font-bold text-2xl  max-w-[300px]'>Mentorship can help you  embrace your potential! </h1>
   <p className='py-2 mb-4 max-w-[300px]'>We all are already abundant. We just need a mentor to help us realise it.</p>
   <Button/>
